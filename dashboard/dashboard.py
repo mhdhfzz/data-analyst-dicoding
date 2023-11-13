71fa5effcb830fc5705d56203a682bc48da0f6e8
Fiz <ciafiz111@gmail.com>
2023-11-14 02:20:32 +0700

    Fix error

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,12 +11,12 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("df.csv")
+all_df = pd.read_csv("https://raw.githubusercontent.com/mhdhfzz/data-analyst-dicoding/main/dashboard/df.csv")
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('geolocation.csv')
+geolocation = pd.read_csv('https://github.com/mhdhfzz/data-analyst-dicoding/blob/main/dashboard/geolocation.csv')
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
@@ -31,7 +31,7 @@ with st.sidebar:
     with col1:
         st.write(' ')
     with col2:
-        st.image("logo.png"
+        st.image("https://github.com/mhdhfzz/data-analyst-dicoding/blob/main/dashboard/logo.png"
                  , width=100)
     with col3:
         st.write(' ')
