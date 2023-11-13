f27bc00c02eb17260e1ed0b7b98080a206b0465e
Fiz <ciafiz111@gmail.com>
2023-11-14 02:15:17 +0700

    Update error

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,12 +11,12 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("./df.csv")
+all_df = pd.read_csv("df.csv")
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('./geolocation.csv')
+geolocation = pd.read_csv('geolocation.csv')
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
@@ -31,7 +31,7 @@ with st.sidebar:
     with col1:
         st.write(' ')
     with col2:
-        st.image("./logo.png"
+        st.image("logo.png"
                  , width=100)
     with col3:
         st.write(' ')
